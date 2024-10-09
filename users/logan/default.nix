d4df3ac86b5001f2f9c0a319ff9687d18ba56d45
= <=>
2024-10-09 11:29:27 -0400

    Add git ignore alias

--- users/logan/default.nix
+++ users/logan/default.nix
@@ -225,6 +225,9 @@ in
       lfs.enable = true;
       userName = "link00000000";
       userEmail = "crandall.logan@gmail.com";
+      aliases = {
+        ignore = "!gi() { curl -sL https://www.toptal.com/developers/gitignore/api/$@ ;}; gi";
+      };
     };
     programs.lazygit = {
       enable = true;
