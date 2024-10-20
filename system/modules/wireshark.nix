6bc7985b5a6814a8fc4909de15449ceda5c4cde4
= <=>
2024-10-19 23:12:54 -0400

    Fix wireshark not being launchable

--- system/modules/wireshark.nix
+++ system/modules/wireshark.nix
@@ -1,5 +1,9 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
+  environment.systemPackages = with pkgs; [
+    wireshark # Required even through the program is enabled throught programs.*
+  ];
+
   programs.wireshark.enable = true;
 }
