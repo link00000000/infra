8cc76d0ac842a45f86e828e1510ba929d1f744da
= <=>
2025-06-30 20:29:49 -0400

    Revert "Bump flake inputs to 24.11"
    This reverts commit 01c524f9bb31d6f6dff413fe798c3de0bcfbbe28.

--- flake.nix
+++ flake.nix
@@ -14,15 +14,15 @@
   };
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
 
-    home-manager.url = "github:nix-community/home-manager/release-24.11";
+    home-manager.url = "github:nix-community/home-manager/release-24.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
     nixos-wsl.url = "github:nix-community/NixOS-WSL/main";
 
-    stylix.url = "github:danth/stylix/release-24.11";
+    stylix.url = "github:danth/stylix/release-24.05";
     stylix.inputs.nixpkgs.follows = "nixpkgs";
 
     apple-fonts.url = "github:Lyndeno/apple-fonts.nix";
