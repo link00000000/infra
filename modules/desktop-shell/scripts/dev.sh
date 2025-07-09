98a49ae250133cc7bd47109f41354cbcd6ede2d2
= <=>
2025-07-09 17:14:09 -0400

    Update script to terminate and restore desktop-shell systemd service if it is active

--- modules/desktop-shell/scripts/dev.sh
+++ modules/desktop-shell/scripts/dev.sh
@@ -2,6 +2,13 @@
 #! nix-shell -i bash -p bash cmake inotify-tools gum
 
 STATE="building"
+SHOULD_RESTORE_SYSTEMD_UNIT=0
+
+export GUM_CONFIRM_PROMPT_FOREGROUND="7"
+export GUM_CONFIRM_SELECTED_FOREGROUND="15"
+export GUM_CONFIRM_SELECTED_BACKGROUND="12"
+export GUM_CONFIRM_UNSELECTED_FOREGROUND="8"
+export GUM_CONFIRM_UNSELECTED_BACKGROUND="15"
 
 if [[ $DEV_DEBUG -eq 1 ]]; then
   set -x
@@ -29,28 +36,55 @@ cleanup() {
   log info "Exiting..."
   pkill --signal TERM --parent "$$"
   wait "$$" 2>/dev/null
+
+  if [[ $SHOULD_RESTORE_SYSTEMD_UNIT -eq 1 ]]; then
+    log info "Restoring systemd unit..."
+    systemctl start --user "desktop-shell.service"
+  fi
+
   exit 0
 }
 
-trap cleanup SIGINT
+main() {
+  trap cleanup SIGINT
 
-while true; do
-  STATE="building"
-  mkdir -p build
+  if systemctl is-active --user --quiet "desktop-shell.service"; then 
+    gum confirm "An existing systemd unit is already running desktop-shell. Terminate it and restore on exit?"
+    local gum_exitcode=$?
 
-  log info "Building..."
-  cmake --build build
+    case $gum_exitcode in
+      0)
+        systemctl stop --user desktop-shell
+        SHOULD_RESTORE_SYSTEMD_UNIT=1
+        ;;
+      1)
+        ;;
+      *)
+        exit $gum_exitcode
+        ;;
+    esac
+  fi
 
-  STATE="running"
-  run &
-  DESKTOPSHELL_PID=$!
+  while true; do
+    STATE="building"
+    mkdir -p build
 
-  log info "Waiting for changes... (^C to quit)"
-  inotifywait --quiet --recursive --event create,modify,delete --exclude '^\./build/' .
-  log info "Change detected, restarting..."
+    log info "Building..."
+    cmake --build build
 
-  STATE="restarting"
-  pkill --signal TERM --parent "$DESKTOPSHELL_PID"
-  kill -TERM "$DESKTOPSHELL_PID"
-  wait "$DESKTOPSHELL_PID"
-done
+    STATE="running"
+    run &
+    DESKTOPSHELL_PID=$!
+
+    log info "Waiting for changes... (^C to quit)"
+    inotifywait --quiet --recursive --event create,modify,delete --exclude '^\./build/' .
+    log info "Change detected, restarting..."
+
+    STATE="restarting"
+    pkill --signal TERM --parent "$DESKTOPSHELL_PID"
+    kill -TERM "$DESKTOPSHELL_PID"
+    wait "$DESKTOPSHELL_PID"
+  done
+}
+
+main
